e9f0d9fe601099d4c8f8f87517544e95ab82bd9c
liflatt <lflatt1221@gmail.com>
2025-06-08 12:53:05 -0500

    Added page sections: heading, main, footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
+    <div className="App mt-5">
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+          <main className="mt-4 d-flex justify-content-center">
+            Main Placeholder
+          </main>
+        </header>
+        <footer className="mt-5">
+          <p>
+            This project was coded by Lindsey Flatt and is{" "}
+            <a
+              href="https://github.com/liflatt/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://lf-dictionary-project.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
